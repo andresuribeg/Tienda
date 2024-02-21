36ad6963c6ae0cbd424af2c58b72fbc1b7835b8b
Andres Uribe <andres.uribe.gutierrez@gmail.com>
2024-02-21 01:27:03 -0500

    Cambio de palabras por iconos en el toolbar v20240214_3

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -7,7 +7,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
