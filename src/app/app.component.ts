0acef9c2756effa6c42a04968f88dd7f8d9409f0
Andres Uribe <andres.uribe.gutierrez@gmail.com>
2024-02-21 00:17:26 -0500

    Se implementan componentes de material al home v20240214_2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MenuComponent } from "./shared/menu/menu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, MenuComponent]
 })
 export class AppComponent {
   title = 'Tienda';
